506ab5911dd9700003bae28663f09ef8b3ae36c7
Benjamín Alamos <benjalamos@uc.cl>
2023-04-17 17:33:04 -0400

    wasm y js creados

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+# Generar módulo Wasm y el archivo Javascript de unión
 
+1   Instalar Emscripten siguiendo las instrucciones en https://emscripten.org/docs/getting_started/downloads.html.
+
+2   Compilar el código en C a WebAssembly generando el codigo Javascritp de unión usando el siguiente comando en la terminal:
+
+    emcc -O3 -s WASM=1 -s MAIN_MODULE=1 -s EXPORTED_FUNCTIONS="['_findOptimalAssignment']" -o optimal-assignment.js optimal-assignment.c
+
+Este comando compila el código en C a WebAssembly con la optimización de nivel 3, habilita la generación de módulos WASM, expone la función findOptimalAssignment y genera el archivo optimal-assignment.wasm y optimal-assignment.js
+El flag -s MAIN_MODULE=1 indica que el módulo wasm generado es un módulo principal y no necesita una función main
+
+3 Limpiar cahce de emcc
+
+    emcc --clear-cache
---
+++ optimal-assignment.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+
+int* findOptimalAssignment(int N, int *jobs, int M, int *result) {
+    // Inicializar las variables
+    int i, j, k, min_time, min_index;
+    int *times = (int *) malloc(M * sizeof(int));
+    int *assigned = (int *) malloc(N * sizeof(int));
+
+    // Asignar todos los trabajos al primer cluster
+    memset(assigned, 0, N * sizeof(int));
+
+    // Asignar los trabajos a los clusters restantes
+    for (i = 1; i < M; i++) {
+        // Calcular los tiempos de ejecución para cada cluster
+        for (j = 0; j < M; j++) {
+            times[j] = 0;
+        }
+        for (j = 0; j < N; j++) {
+            min_time = times[0] + jobs[j];
+            min_index = 0;
+            for (k = 1; k < M; k++) {
+                if (times[k] + jobs[j] < min_time) {
+                    min_time = times[k] + jobs[j];
+                    min_index = k;
+                }
+            }
+            times[min_index] += jobs[j];
+            assigned[j] = min_index;
+        }
+
+        // Copiar los resultados en el array de salida
+        for (j = 0; j < N; j++) {
+            result[i * N + j] = assigned[j];
+        }
+    }
+
+    return result;
+
+    // Liberar la memoria
+    free(times);
+    free(assigned);
+}
