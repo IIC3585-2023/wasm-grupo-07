fe5195d396fe227a8940a545d0f50a16a998418d
Benjamín Alamos <benjalamos@uc.cl>
2023-04-20 11:12:30 -0400

    js algoritmo 2

--- README.md
+++ README.md
@@ -4,15 +4,13 @@
 
 2   Compilar el código en C a WebAssembly generando el codigo Javascritp de unión usando el siguiente comando en la terminal:
 
-    emcc -O3 -s WASM=1 -s MAIN_MODULE=1 -s EXPORTED_RUNTIME_METHODS= "['cwrap', 'ccall']" -o optimal-assignment.js optimal-assignment.c
+    emcc -O3 -s EXPORTED_FUNCTIONS=_task_assignment,_malloc,_free -s EXPORTED_RUNTIME_METHODS=ccall -o main.js algoritmo2.c
 
 "-O3": habilita la optimización de nivel 3.
-"-s WASM=1": especifica que se debe generar un módulo WebAssembly en lugar de JavaScript.
-"-s MAIN_MODULE=1": indica que se debe generar un módulo WebAssembly que se cargará como un módulo principal. Esto permite que el módulo sea utilizado como una biblioteca desde otros módulos WebAssembly.
-"-s EXPORTED_RUNTIME_METHODS=['cwrap', 'ccall']": exporta los métodos "cwrap" y "ccall" de Emscripten, que permiten llamar a funciones C desde JavaScript. Dónde:
+"-s EXPORTED_RUNTIME_METHODS=ccall: exporta el método "ccall" de Emscripten, que permiten llamar a funciones C desde JavaScript. Dónde:
 **ccall**: calls a compiled C function with the specified variables and return the result.
 **cwrap**: "wraps" a compiled C function and returns a JavaScript function you can call normally. That is, by far, more useful and we will focus on this method.
-"-o optimal-assignment.js optimal-assignment.c": indica que el resultado de la compilación debe ser un archivo JavaScript llamado "optimal-assignment.js" y se basa en el archivo fuente "optimal-assignment.c".
+"-o main.js algoritmo2.c": indica que el resultado de la compilación debe ser un archivo JavaScript llamado "main.js" y se basa en el archivo fuente "algoritmo2.c".
 
 3 Limpiar cahce de emcc
 
--- index.js
+++ index.js
@@ -1,54 +1,106 @@
+const INY_BYTES = 4 //4 bytes por cada elemento (entero de 32 bits)
 
+
+//------//
+// Utilidades //
+//------//
+function setHtml(clusters) {
+    // Mostrar el resultado en la página
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = '';
+    for (var i = 0; i < M; i++) {
+        outputDiv.innerHTML +=
+        '<p>Cluster ' +
+        (i + 1) +
+        ': ' +
+        clusters.slice(i * N, (i + 1) * N).join(', ') +
+        '</p>';
+    }
+}
+
+function getData(){
+    // Obtener los datos de entrada del formulario
+    var N = parseInt(document.getElementById('numJobs').value);
+    var jobs = document
+        .getElementById('jobDurations')
+        .value.split(',')
+        .map(function (x) {
+            return parseInt(x);
+        });
+    var M = parseInt(document.getElementById('numClusters').value);
+    return [N,jobs,M]
+}
+
+
+//------//
+// WASM //
+//------//
 // Wait for WASM compilation.
 Module.onRuntimeInitialized = () => {
-    const { task_assignment } = Module;
-  
-    //------//
-    // WASM //
-    //------//
-    document.getElementById('execute_btn').onclick = () => {
-        // Obtener los datos de entrada del formulario
-        var N = parseInt(document.getElementById('numJobs').value);
-        var jobs = document
-            .getElementById('jobDurations')
-            .value.split(',')
-            .map(function (x) {
-                return parseInt(x);
-            });
-        var M = parseInt(document.getElementById('numClusters').value);
-            
+    document.getElementById('execute_wasm_btn').onclick = () => {
+        
+        [N,jobs,M] = getData()
         console.log('Estos son los datos de entrada:', N, jobs, M);
         
-        const jobsPtr = Module._malloc(N * 4);  // 4 bytes por cada elemento (entero de 32 bits)
-        Module.HEAP32.set(jobs, jobsPtr / 4);
+        const jobsPtr = Module._malloc(N * INY_BYTES);
+        Module.HEAP32.set(jobs, jobsPtr / INY_BYTES);
 
         const clustersPtr = Module.ccall("task_assignment", "number",
         ["number", "number", "number"],
         [N, jobsPtr, M]);
 
-        const clusters = Module.HEAP32.subarray(clustersPtr / 4, clustersPtr / 4 + N*M);
+        const clusters = Module.HEAP32.subarray(clustersPtr / INY_BYTES, clustersPtr / INY_BYTES + N);
         
         // Liberar la memoria reservada para `t` y `clusters`
         Module._free(jobsPtr);
         Module._free(clustersPtr);
 
         console.log(clusters);
-        
-        // findOptimalAssignment(N, jobs, M, outputArray.byteOffset);
-        // console.log(`[${outputArray.join(", ")}]`);
-        
-
-        // Mostrar el resultado en la página
-        var outputDiv = document.getElementById('output');
-        outputDiv.innerHTML = '';
-        for (var i = 0; i < M; i++) {
-            outputDiv.innerHTML +=
-            '<p>Cluster ' +
-            (i + 1) +
-            ': ' +
-            clusters.slice(i * N, (i + 1) * N).join(', ') +
-            '</p>';
-        }
+        setHtml(clusters)
     }
-
 }
+
+
+//------//
+// JS Algoritmo 2//
+//------//
+document.getElementById('execute_js_btn').onclick = () => {
+    [N,jobs,M] = getData()
+    const clusters = task_assignment(N, jobs, M)
+    console.log(clusters);
+    setHtml(clusters)
+}
+
+function task_assignment(N, Ti, M) {
+    let Ci = Array.from(Array(M), () => Array(N).fill(-1));
+    let tiempos = Array(M).fill(0);
+  
+    for (let i = 0; i < N; i++) {
+      let min_tiempo = tiempos[0];
+      let min_cluster = 0;
+  
+      for (let j = 1; j < M; j++) {
+        if (tiempos[j] < min_tiempo) {
+          min_tiempo = tiempos[j];
+          min_cluster = j;
+        }
+      }
+  
+      Ci[min_cluster][i] = 1;
+      tiempos[min_cluster] += Ti[i];
+    }
+  
+    let lista_asignacion = [];
+  
+    for (let i = 0; i < N; i++) {
+      let cluster = 0;
+  
+      for (let j = 0; j < M; j++) {
+        if (Ci[j][i] !== -1) {
+          cluster = j + 1;
+          lista_asignacion[i] = cluster;
+        }
+      }
+    }
+    return lista_asignacion;
+  }
